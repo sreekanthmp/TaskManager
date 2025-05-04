1a91dc29098b61f7da6c35837c3af79e0bc8d10a
sreekanth-bochdale <sreekanth@bochdale.com>
2025-05-04 12:30:02 +0530

    updated settings

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-#90w!2j!5yb=tb)j4@xsi+jg$3wlb7ae!(umygindlo5gga@81
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
